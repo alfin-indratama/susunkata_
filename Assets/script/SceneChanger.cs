3801d673686fc12634fd02d91a0f583a27b926e2
alfin-indratama <alfin1900018108@webmail.uad.ac.id>
2023-05-25 15:04:15 +0700

    first commit

---
+++ Assets/script/SceneChanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+	public void gantiScene(int sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
